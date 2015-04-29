c2da72427cf3253c6f29cf8a5b683f9c1654b9de
Logan-K <lklingler0@gmail.com>
2015-04-28 22:25:06 -0400

    added email and password fields to user

--- db/schema.rb
+++ db/schema.rb
@@ -11,13 +11,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150429015537) do
+ActiveRecord::Schema.define(version: 20150429021036) do
 
   create_table "users", force: :cascade do |t|
     t.string   "firstname"
     t.string   "lastname"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string   "email"
+    t.string   "password"
   end
 
 end
