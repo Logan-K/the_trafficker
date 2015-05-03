04048bdd6fec59820765e646628f81846d92a83b
Sparc0 <lklingler0@gmail.com>
2015-05-03 16:22:44 -0400

    Added validations and error messages to user

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,6 @@
 class User < ActiveRecord::Base
+  validates_presence_of :firstname, :message => "First Name can't be blank"
+  validates_presence_of :lastname, :message => "Last Name can't be blank"
+  validates_format_of :email, with: /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/i, on: :create , :message=> "Must Enter a Valid Email Adress"
+  validates_length_of :password, in: 5..20 , :message => "Password must be between 5 and 20 characters"
 end
