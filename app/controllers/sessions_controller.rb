0a2a0d7ad18cd0fcbc72aa9a8b0d61db2269a0e7
Sparc0 <lklingler0@gmail.com>
2015-05-03 22:31:24 -0400

    Added bcrypt gem and got further with login system

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,4 +1,23 @@
 class SessionsController < ApplicationController
   def new
   end
+
+  def create
+    user = User.find_by(email: params[:session][:email].downcase)
+    #user = User.where(:email => params[:session][:email].downcase).first
+    if user && user.authenticate(params[:session][:password])
+      log_in user
+      redirect_to user
+      #redirect to successful login page (whatever it may be)
+    else
+      #error
+      flash[:danger] = "Invalid email/password combination"
+      render 'new'
+    end
+
+  end
+
+  def destroy
+  end
+
 end
