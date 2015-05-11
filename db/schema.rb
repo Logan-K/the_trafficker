5f3d12fdc3a34238844c9dfda228ca85e6eae3c7
Logan-K <lklingler0@gmail.com>
2015-05-10 20:35:04 -0400

    Changed user model to try and correctly use password_digest

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150510191741) do
+ActiveRecord::Schema.define(version: 20150510233454) do
 
   create_table "elements", force: :cascade do |t|
     t.string   "description"
@@ -30,7 +30,6 @@ ActiveRecord::Schema.define(version: 20150510191741) do
     t.datetime "created_at",      null: false
     t.datetime "updated_at",      null: false
     t.string   "email"
-    t.string   "password"
     t.string   "password_digest"
   end
 
